b89d028157472bb4b516fa7d12ca9e96ada584ab
SwiftKobe <folledoswag@gmail.com>
2018-06-21 21:10:46 -0400

    - Added a new file for Feed TableViewController and a touch class to control the cell - Added a way to access photos from the photoLibrary and add a comment to it and save it to the parse server - Added a way for the current user to view other user's post that they are following with the image, comments, and username of the person who upload that post - Also added a way to save an image as a PFFile into the server

--- FolledoInstagramClone/FeedTableViewController.swift
+++ FolledoInstagramClone/FeedTableViewController.swift
@@ -6,90 +6,117 @@
 //  Copyright © 2018 Samuel Folledo. All rights reserved.
 //
 
+//1 Parse Server & AWS
+//2 Retrieving & Updating Data
+//3 Accessing the Camera Roll
+//4 Spinners & Alerts
+//5 Login & Signup
+//6 User Table
+//7 Pull to Refresh
+//8 Posting Images
+//9 Viewing User's Feeds
+
 import UIKit
+import Parse
 
 class FeedTableViewController: UITableViewController {
 
+    var users = [String: String]()
+    var comments = [String]() //17mins
+    var usernames = [String]() //17mins
+    var imageFiles = [PFFile]() //17mins
+    
+//viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let query = PFUser.query()
+        query?.whereKey("username", notEqualTo: PFUser.current()?.username) //10mins so we dont see the current username
+        query?.findObjectsInBackground(block: { (objects,error) in
+            
+            if let users = objects { //objects contains users but are initially PFObjects that is why we have to unwrap them as users
+                for object in users { //now that we have unwrap them, we can loop through them
+                    if let user = object as? PFUser { //11mins initially we called them objects, but then we created a user from them by casting the object from PFObject to PFUser
+                        self.users[user.objectId!] = user.username! //12mins and store username and userId together
+                    }
+                }
+            }
+            
+            //13mins now we want to workout who our current user follows
+            let getFollowedUsersQuery = PFQuery(className: "Following")
+            getFollowedUsersQuery.whereKey("follower", equalTo: PFUser.current()?.objectId) //13mins this finds all the users our current user follows
+            getFollowedUsersQuery.findObjectsInBackground(block: { (objects, error) in //runs the query
+                if let followers = objects { //14mins if object contains anything, it will be a bunch of followers
+                    for follower in followers {
+                        if let followedUser = follower["following"] {//15mins we want to find who the "following" userId is to get a list of all the people that our user is following
+                            let query = PFQuery(className: "Post") //then find the posts of that user
+                            query.whereKey("userId", equalTo: followedUser) //16mins we want it to be equal to the userId of the user we're checking the post of
+                            query.findObjectsInBackground(block: { (objects, error) in
+                                if let posts = objects {
+                                    for post in posts { //16mins
+                                        self.comments.append(post["message"] as! String) //18mins RECOMMENDED to use if-let when uploading the app, but for now we'll use force unwrap
+                                        self.usernames.append(self.users[post["userId"] as! String]!) //18mins
+                                        self.imageFiles.append(post["imageFile"] as! PFFile) //18mins
+                                        
+                                        self.tableView.reloadData()//19mins now we reloadData
+                                    }
+                                }
+                            })
+                            
+                        }
+                        
+                    }
+                }
+            })
+        })
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
+//numberOfSections
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
+//numberOfRows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return comments.count
     }
 
-    /*
+//cellForRowAt
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedTableViewCell //9 //5mins "Cell" is the name of cell identifier //we have to force cast it of FeedTableViewCell and we know it will work because they are already linked
+        
+/*  //7 mins We can access any outlets within the cell by using cell dot notation
+        cell.postedImage.image = UIImage(named:"museum.jpg")
+        cell.comment.text = "Comment"
+        cell.userInfo.text = "Username"
+*/
+        
+        //21mins now we download those images
+        imageFiles[indexPath.row].getDataInBackground { (data, error) in
+            if let imageData = data {
+                //22mins then we add the imageData to our cell.postedImage
+                if let imageToDisplay = UIImage(data: imageData) { //22mins
+                    
+                    cell.postedImage.image = imageToDisplay //22mins
+                }
+            }
+        }
+        
+        //cell.postedImage.image = UIImage(named: "museum.jpg") //20mins
+        cell.comment.text = comments[indexPath.row] //20mins
+        cell.userInfo.text = usernames[indexPath.row] //20mins
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
--- FolledoInstagramClone/PostViewController.swift
+++ FolledoInstagramClone/PostViewController.swift
@@ -38,47 +38,46 @@ class PostViewController: UIViewController, UINavigationControllerDelegate, UIIm
         
     }
 //8 didFinishPickingMediaWithInfo method for updating the image as well
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) { //dont forget to also update info.plist's privacy to allow photo library usage
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) { //8 //dont forget to also update info.plist's privacy to allow photo library usage
         
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {//9mins this is before we check to see whether we can create a var image if the user has chosen the image and not cancel and cast it as UIImage
-            imageToPost.image = image
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage { //8 //9mins this is before we check to see whether we can create a var image if the user has chosen the image and not cancel and cast it as UIImage
+            imageToPost.image = image //8
         }
-        self.dismiss(animated: true, completion: nil) //10mins
+        self.dismiss(animated: true, completion: nil) //8 //10mins
     }
     
     @IBAction func postImage(_ sender: Any) { //8
         
-        if let image = imageToPost.image { //14 mins imageData is an optional so first check if there is an image
-            let post = PFObject(className: "Post") //create a new class
-            post["message"] = comment.text
-            post["userId"] = PFUser.current()?.objectId
+        if let image = imageToPost.image { //8 //14 mins imageData is an optional so first check if there is an image
+            let post = PFObject(className: "Post") //8 //create a new class
+            post["message"] = comment.text //8
+            post["userId"] = PFUser.current()?.objectId //8
             
-            if let imageData = UIImagePNGRepresentation(image) {
-                
+            if let imageData = UIImagePNGRepresentation(image) { //8
             //8 run a spinner while uploading image and comment
                 //activityIndicator
-                let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50)) //4 create a spinner with height and width of 50
-                activityIndicator.center = self.view.center //5
-                activityIndicator.hidesWhenStopped = true //5
-                activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray //5
-                view.addSubview(activityIndicator) //5
-                activityIndicator.startAnimating() //5
-                UIApplication.shared.beginIgnoringInteractionEvents() //5 pauses the app when its loading
+                let activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50)) //8 create a spinner with height and width of 50
+                activityIndicator.center = self.view.center //5 //8
+                activityIndicator.hidesWhenStopped = true //5 //8
+                activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray //5 //8
+                view.addSubview(activityIndicator) //5 //8
+                activityIndicator.startAnimating() //5 //8
+                UIApplication.shared.beginIgnoringInteractionEvents() //5 //8 pauses the app when its loading
                 
                 
-                let imageFile = PFFile(name: "image.png", data: imageData)
-                post["imageFile"] = imageFile
-                post.saveInBackground(block: { (success, error) in
+                let imageFile = PFFile(name: "image.png", data: imageData) //8
+                post["imageFile"] = imageFile //8
+                post.saveInBackground(block: { (success, error) in //8
                     
-                   activityIndicator.stopAnimating() //stop spinner after it saveInBackground method runs
-                    UIApplication.shared.endIgnoringInteractionEvents()
+                   activityIndicator.stopAnimating() //8 //stop spinner after it saveInBackground method runs
+                    UIApplication.shared.endIgnoringInteractionEvents() //8
                     
-                    if success {
-                        self.displayAlert(title: "Image Posted", message: "Your image has been posted successfully")
-                        self.comment.text = "" //17mins clear textfield and imageview
-                        self.imageToPost.image = nil
-                    } else { //if something goes wrong
-                        self.displayAlert(title: "Image Could Not Be Posted", message: "Please try again later")
+                    if success { //8
+                        self.displayAlert(title: "Image Posted", message: "Your image has been posted successfully") //8
+                        self.comment.text = "" //8 //17mins clear textfield and imageview
+                        self.imageToPost.image = nil //8
+                    } else { //8 //if something goes wrong
+                        self.displayAlert(title: "Image Could Not Be Posted", message: "Please try again later") //8
                     }
                 })
             }
@@ -97,13 +96,13 @@ class PostViewController: UIViewController, UINavigationControllerDelegate, UIIm
         // Dispose of any resources that can be recreated.
     }
     
-//displayAlert method
-    func displayAlert(title:String, message:String) { //5
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert) //5
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in //5
-            self.dismiss(animated: true, completion: nil) //5
-        })) //5
-        self.present(alert, animated: true, completion: nil) //5
-    } //5
+//8 displayAlert method
+    func displayAlert(title:String, message:String) { //5 //8
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert) //5 //8
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in //5 //8
+            self.dismiss(animated: true, completion: nil) //5 //8
+        })) //5 //8
+        self.present(alert, animated: true, completion: nil) //5 //8
+    } //5 //8
     
 }
